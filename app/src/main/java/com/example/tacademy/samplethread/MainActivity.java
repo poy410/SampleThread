a98d8f66f24d8482011d7cf8fee665f56374d446
poy410 <poy410@naver.com>
2016-04-22 10:33:23 +0900

    h

--- app/src/main/java/com/example/tacademy/samplethread/MainActivity.java
+++ app/src/main/java/com/example/tacademy/samplethread/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tacademy.samplethread;
 
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -91,6 +92,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    class MyTask extends AsyncTask<String,Integer,Boolean>{
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+        }
 
+        @Override
+        protected Boolean doInBackground(String... params) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+    }
 }
 
